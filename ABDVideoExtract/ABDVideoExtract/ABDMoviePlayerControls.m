12842c9725ccebf138e82a92ab7b92558bf2b030
Seungwon Kim <closer527@gmail.com>
2015-01-29 14:19:11 +0900

    커스텀 플레이어를 위한 클래스 생성

---
+++ ABDVideoExtract/ABDVideoExtract/ABDMoviePlayerControls.m
@@ -0,0 +1,13 @@
+//
+//  ABDMoviePlayerControls.m
+//  ABDVideoExtract
+//
+//  Created by Seung-won Kim on 1/29/15.
+//  Copyright (c) 2015 anbado video. All rights reserved.
+//
+
+#import "ABDMoviePlayerControls.h"
+
+@implementation ABDMoviePlayerControls
+
+@end
