e67a214b9da3dcbecf080257389c31cc7bc3e801
Seungwon Kim <closer527@gmail.com>
2015-01-29 14:44:49 +0900

    비디오 타임라인을 위한 커스텀 슬라이더 클래스 생성

---
+++ ABDVideoExtract/ABDVideoExtract/ABDExtractSlider.h
@@ -0,0 +1,13 @@
+//
+//  ABDExtractSlider.h
+//  ABDVideoExtract
+//
+//  Created by Seung-won Kim on 1/29/15.
+//  Copyright (c) 2015 anbado video. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABDExtractSlider : UISlider
+
+@end
---
+++ ABDVideoExtract/ABDVideoExtract/ABDExtractSlider.m
@@ -0,0 +1,13 @@
+//
+//  ABDExtractSlider.m
+//  ABDVideoExtract
+//
+//  Created by Seung-won Kim on 1/29/15.
+//  Copyright (c) 2015 anbado video. All rights reserved.
+//
+
+#import "ABDExtractSlider.h"
+
+@implementation ABDExtractSlider
+
+@end
