24e271ded893e91bfa65d9683e6e42b06e188379
Seungwon Kim <closer527@gmail.com>
2015-01-29 14:29:22 +0900

    엑기스 영역을 위한 클래스 생성

---
+++ ABDVideoExtract/ABDVideoExtract/ExtractSection.m
@@ -0,0 +1,13 @@
+//
+//  ExtractSection.m
+//  ABDVideoExtract
+//
+//  Created by Seung-won Kim on 1/29/15.
+//  Copyright (c) 2015 anbado video. All rights reserved.
+//
+
+#import "ExtractSection.h"
+
+@implementation ExtractSection
+
+@end
