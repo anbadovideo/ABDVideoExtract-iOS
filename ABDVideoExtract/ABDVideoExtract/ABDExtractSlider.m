cb4a9e85849087a41db0ad08b72c0c07c4097e53
Seungwon Kim <closer527@gmail.com>
2015-01-29 18:06:52 +0900

    엑기스 구간 슬라이더에 표시하도록 구현

--- ABDVideoExtract/ABDVideoExtract/ABDExtractSlider.m
+++ ABDVideoExtract/ABDVideoExtract/ABDExtractSlider.m
@@ -7,6 +7,13 @@
 //
 
 #import "ABDExtractSlider.h"
+#import "ExtractSection.h"
+
+const int kHeightOfSlider = 44;
+
+@interface ABDExtractSlider ()
+@property (nonatomic, strong) NSArray *markPositions;
+@end
 
 @implementation ABDExtractSlider
 
@@ -42,4 +49,86 @@
     [self setMinimumTrackImage:[UIImage imageNamed:@"slider_active.png"] forState:UIControlStateNormal];
     [self setThumbImage:[UIImage imageNamed:@"slider_thumb.png"] forState:UIControlStateNormal];
 }
+
+- (void)setMaximumValue:(float)maximumValue {
+    [super setMaximumValue:maximumValue];
+
+    // override setMaximumValue:
+    _duration = maximumValue;
+    [self setNeedsDisplay];
+}
+
+- (void)setDuration:(NSTimeInterval)duration {
+    _duration = duration;
+    [self setNeedsDisplay];
+}
+
+- (void)setExtractSections:(NSArray *)extractSections {
+    // Todo : duration이 0이면 exception 처리.
+    _extractSections = extractSections;
+    [self setNeedsDisplay];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+
+    // We create an innerRect in which we paint the lines
+    CGRect innerRect = CGRectInset(rect, 0.0, 00.0);
+
+    UIGraphicsBeginImageContextWithOptions(innerRect.size, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    // Selected side
+    CGContextSetLineWidth(context, kHeightOfSlider);
+    CGContextMoveToPoint(context, 0, CGRectGetHeight(innerRect)/2);
+    CGContextAddLineToPoint(context, innerRect.size.width, CGRectGetHeight(innerRect)/2);
+    CGContextSetStrokeColorWithColor(context, [[UIColor lightGrayColor] CGColor]);
+    CGContextStrokePath(context);
+    UIImage *selectedSide = [UIGraphicsGetImageFromCurrentImageContext() resizableImageWithCapInsets:UIEdgeInsetsZero];
+
+    // Unselected side
+    CGContextSetLineWidth(context, kHeightOfSlider);
+    CGContextMoveToPoint(context, 0, CGRectGetHeight(innerRect)/2);
+    CGContextAddLineToPoint(context, innerRect.size.width, CGRectGetHeight(innerRect)/2);
+    CGContextSetStrokeColorWithColor(context, [[UIColor darkGrayColor] CGColor]);
+    CGContextStrokePath(context);
+    UIImage *unselectedSide = [UIGraphicsGetImageFromCurrentImageContext() resizableImageWithCapInsets:UIEdgeInsetsZero];
+
+    // duration이 0일때는 일단 패스.
+    if (_duration <= 0)
+        return;
+
+    // Set trips on selected side
+    [selectedSide drawAtPoint:CGPointMake(0,0)];
+    for (int i = 0; i < [_extractSections count]; i++) {
+        CGContextSetLineWidth(context, kHeightOfSlider);
+        float startPosition = (float)([_extractSections[i] startTime] / _duration) * self.frame.size.width;
+        float endPosition = (float)([_extractSections[i] endTime] / _duration) * self.frame.size.width;
+        CGContextMoveToPoint(context, startPosition, CGRectGetHeight(innerRect)/2);
+        CGContextAddLineToPoint(context, endPosition, CGRectGetHeight(innerRect)/2);
+        CGContextSetStrokeColorWithColor(context, [[UIColor blueColor] CGColor]);
+        CGContextStrokePath(context);
+    }
+    UIImage *selectedStripSide = [UIGraphicsGetImageFromCurrentImageContext() resizableImageWithCapInsets:UIEdgeInsetsZero];
+
+    // Set trips on unselected side
+    [unselectedSide drawAtPoint:CGPointMake(0,0)];
+    for (int i = 0; i < [_extractSections count]; i++) {
+        CGContextSetLineWidth(context, kHeightOfSlider);
+        float startPosition = (float)([_extractSections[i] startTime] / _duration) * self.frame.size.width;
+        float endPosition = (float)([_extractSections[i] endTime] / _duration) * self.frame.size.width;
+        CGContextMoveToPoint(context, startPosition, CGRectGetHeight(innerRect)/2);
+        CGContextAddLineToPoint(context, endPosition, CGRectGetHeight(innerRect)/2);
+        CGContextSetStrokeColorWithColor(context, [[UIColor cyanColor] CGColor]);
+        CGContextStrokePath(context);
+    }
+    UIImage *unselectedStripSide = [UIGraphicsGetImageFromCurrentImageContext() resizableImageWithCapInsets:UIEdgeInsetsZero];
+
+    UIGraphicsEndImageContext();
+
+    [self setMinimumTrackImage:selectedStripSide forState:UIControlStateNormal];
+    [self setMaximumTrackImage:unselectedStripSide forState:UIControlStateNormal];
+}
+
 @end
