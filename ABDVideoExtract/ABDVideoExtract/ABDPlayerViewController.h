793afdbb097e594811673b5deafb539ae99dcd63
Seungwon Kim <closer527@gmail.com>
2015-02-04 16:55:33 +0900

    AVPlayer를 이용한 플레이어뷰를 위한 클래스 추가

---
+++ ABDVideoExtract/ABDVideoExtract/ABDPlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABDPlayerViewController.h
+//  ABDVideoExtract
+//
+//  Created by Seung-won Kim on 2/4/15.
+//  Copyright (c) 2015 anbado video. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABDPlayerViewController : UIViewController
+
+@end
---
+++ ABDVideoExtract/ABDVideoExtract/ABDPlayerViewController.m
@@ -0,0 +1,14 @@
+//
+//  ABDPlayerViewController.m
+//  ABDVideoExtract
+//
+//  Created by Seung-won Kim on 2/4/15.
+//  Copyright (c) 2015 anbado video. All rights reserved.
+//
+
+#import "ABDPlayerViewController.h"
+#import "ABDExtractSlider.h"
+
+@implementation ABDPlayerViewController
+
+@end
