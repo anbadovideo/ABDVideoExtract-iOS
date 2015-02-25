bc4d3a9a9580d11223e4998ec3dd3bc5142c62af
Seungwon Kim <closer527@gmail.com>
2015-02-25 11:27:23 +0900

    멀티디바이스 대응(아이폰,아이패드) 테이블뷰 구성, 기본적인 레이아웃 배치

--- ABDVideoExtract/ABDVideoExtract/ViewController.m
+++ ABDVideoExtract/ABDVideoExtract/ViewController.m
@@ -8,6 +8,25 @@
 
 #import "ViewController.h"
 
+@interface ABDEkisuCell : UITableViewCell
+@property (nonatomic, strong) IBOutlet UIImageView *ekisuThumbnailImageView;
+@property (nonatomic, strong) IBOutlet UILabel *ekisuTitleLabel;
+@property (nonatomic, strong) IBOutlet UIView *ekisuRateView;
+@end
+
+@implementation ABDEkisuCell
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+
+    [self.contentView updateConstraintsIfNeeded];
+    [self.contentView layoutIfNeeded];
+
+    self.ekisuTitleLabel.preferredMaxLayoutWidth = CGRectGetWidth(self.ekisuTitleLabel.frame);
+    [self.ekisuTitleLabel sizeToFit];
+}
+@end
+
 @interface ViewController ()
 
 @end
@@ -17,10 +36,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}   
+
+    _ekisus = @[@"1oDAuUx3m6U", @"1oDAuUx3m6U"];
+
+//    self.tableView.estimatedRowHeight = 314.0;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
+{
+    // Do view manipulation here.
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+}
+
+#pragma mark - TableView DataSource
+
+- (CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewAutomaticDimension;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewAutomaticDimension;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [_ekisus count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifer = @"ABDEkisuCell";
+    ABDEkisuCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifer forIndexPath:indexPath];
+
+    [cell.ekisuThumbnailImageView setImage:[UIImage imageNamed:@"slider_active@2x.png"]];
+    [cell.contentView updateConstraintsIfNeeded];
+    [cell.contentView layoutIfNeeded];
+
+    cell.ekisuTitleLabel.preferredMaxLayoutWidth = CGRectGetWidth(cell.ekisuTitleLabel.frame);
+    [cell.ekisuTitleLabel sizeToFit];
+
+    return cell;
+}
+
+#pragma mark - TableView Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+}
+
+- (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+}
+
+
 @end
